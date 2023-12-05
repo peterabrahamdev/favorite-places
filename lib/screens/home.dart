32d2349daae11f3c803edb4e5ef15694f953d868
Peter Abraham <peterabrahamdev@gmail.com>
2023-12-05 19:01:35 +0100

    feat(home): handle having and not having any data

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,14 +1,18 @@
+import 'package:favorite_places/models/place.dart';
+import 'package:favorite_places/providers/place_provider.dart';
 import 'package:favorite_places/screens/add_new_place.dart';
+import 'package:favorite_places/widgets/place_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends ConsumerState<HomeScreen> {
   void _addNewPlace() {
     Navigator.of(context).push(
       MaterialPageRoute(
@@ -17,8 +21,22 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
+  Widget _emptyBody() {
+    return Center(
+        child: Text(
+      'No places added yet',
+      style:
+          Theme.of(context).textTheme.bodySmall!.copyWith(color: Colors.white),
+    ));
+  }
+
+  Widget _dataBody(List<Place> places) {
+    return PlaceList(places: places);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final places = ref.watch(placeProvider);
     return Scaffold(
       appBar: AppBar(title: const Text('Your places'), actions: [
         IconButton(
@@ -26,15 +44,7 @@ class _HomeScreenState extends State<HomeScreen> {
           icon: const Icon(Icons.add),
         ),
       ]),
-      body: Center(
-        child: Text(
-          'No places added yet',
-          style: Theme.of(context)
-              .textTheme
-              .bodySmall!
-              .copyWith(color: Colors.white),
-        ),
-      ),
+      body: places.isEmpty ? _emptyBody() : _dataBody(places),
     );
   }
 }
