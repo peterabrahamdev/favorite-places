95bc64544a10c57d3a7dc3968352adda645e9ea5
Peter Abraham <peterabrahamdev@gmail.com>
2023-12-05 20:58:14 +0100

    feat(widgets): add leading image icon to PlaceItem widget

--- lib/widgets/place_item.dart
+++ lib/widgets/place_item.dart
@@ -9,6 +9,10 @@ class PlaceItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      leading: CircleAvatar(
+        radius: 26,
+        backgroundImage: FileImage(place.image),
+      ),
       title: Text(place.title),
       onTap: () => Navigator.of(context).push(MaterialPageRoute(
         builder: (context) => PlaceDetailsScreen(place: place),
