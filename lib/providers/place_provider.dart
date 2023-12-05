95f93d13d6d43f40ad186950c6e7af646be33151
Peter Abraham <peterabrahamdev@gmail.com>
2023-12-05 18:22:42 +0100

    chore(providers): rename NewPlaceProvider to PlaceProvider

--- lib/providers/place_provider.dart
+++ lib/providers/place_provider.dart
@@ -1,11 +1,14 @@
 import 'package:favorite_places/models/place.dart';
-import 'package:riverpod/riverpod.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class PlaceNotifier extends StateNotifier<List<Place>> {
   PlaceNotifier() : super([]);
 
   void addPlace(Place place) {
     state = [...state, place];
+    for (Place place in state) {
+      print(place.title);
+    }
   }
 }
 
