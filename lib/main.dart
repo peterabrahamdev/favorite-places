e333aa3e15a1ac9fe89e7448b3765ea61d028035
Peter Abraham <peterabrahamdev@gmail.com>
2023-12-05 17:28:02 +0100

    style(ui): create custom color scheme to the application

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+final colorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 102, 6, 247),
+  background: const Color.fromARGB(255, 56, 49, 66),
+);
+
 void main() {
   runApp(const MyApp());
 }
