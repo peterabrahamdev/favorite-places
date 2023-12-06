5ec0786a85635c2e89d89eb9baf942bfbfbee46f
Peter Abraham <peterabrahamdev@gmail.com>
2023-12-06 12:51:20 +0100

    feat(screens): add address and image preview to PlaceDetails screen

--- lib/screens/place_details.dart
+++ lib/screens/place_details.dart
@@ -1,21 +1,66 @@
 import 'package:favorite_places/models/place.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class PlaceDetailsScreen extends StatelessWidget {
   const PlaceDetailsScreen({super.key, required this.place});
 
   final Place place;
 
+  String get locationImage {
+    final lat = place.location.latitude;
+    final lng = place.location.longitude;
+    final key = dotenv.env['GOOGLE_MAPS_API_KEY'];
+
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng=&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:S%7C$lat,$lng&key=$key';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(place.title)),
-      body: Stack(children: [Image.file(
-        place.image,
-        fit: BoxFit.cover,
-        width: double.infinity,
-        height: double.infinity,
-      )],)
-    );
+        appBar: AppBar(title: Text(place.title)),
+        body: Stack(
+          children: [
+            Image.file(
+              place.image,
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
+            ),
+            Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      radius: 70,
+                      backgroundImage: NetworkImage(locationImage),
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 24, vertical: 16),
+                      decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [
+                            Colors.transparent,
+                            Colors.black54,
+                          ],
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                        ),
+                      ),
+                      child: Text(
+                        place.location.address,
+                        style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                              color: Theme.of(context).colorScheme.onBackground,
+                            ),
+                      ),
+                    ),
+                  ],
+                ))
+          ],
+        ));
   }
 }
